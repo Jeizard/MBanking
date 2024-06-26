6de76e42381b82bc3899c8ca1cae1fcffc85bdfb
Jeizard <japhex@mail.ru>
2024-06-26 17:13:17 +0300

    feat: add All Transactions screen to Navigation

--- app/src/main/java/com/jeizard/mbanking/navigation/AppNavHost.kt
+++ app/src/main/java/com/jeizard/mbanking/navigation/AppNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.jeizard.mbanking.ui.screens.all_transactions_screen.AllTransactionsScreen
 import com.jeizard.mbanking.ui.screens.main_screen.MainScreen
 
 @Composable
@@ -17,6 +18,9 @@ fun AppNavHost(
     ) {
         composable(NavigationItem.Main.route) {
             MainScreen()
+        }
+        composable(NavigationItem.AllTransactions.route) {
+            AllTransactionsScreen()
         }
     }
 }
--- app/src/main/java/com/jeizard/mbanking/navigation/AppNavigation.kt
+++ app/src/main/java/com/jeizard/mbanking/navigation/AppNavigation.kt
@@ -1,8 +1,10 @@
 package com.jeizard.mbanking.navigation
 
 enum class Screen {
-    MAIN
+    MAIN,
+    ALLTRANSACTIONS
 }
 sealed class NavigationItem(val route: String) {
     data object Main : NavigationItem(Screen.MAIN.name)
+    data object AllTransactions : NavigationItem(Screen.ALLTRANSACTIONS.name)
 }
