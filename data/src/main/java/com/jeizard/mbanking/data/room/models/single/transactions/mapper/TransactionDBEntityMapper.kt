7290571811464ee9312c337188ee94ee304d3bc6
Jeizard <japhex@mail.ru>
2024-07-04 13:11:59 +0300

    fix: unnecessary parameter in TransactionDBEntityMapper

--- data/src/main/java/com/jeizard/mbanking/data/room/models/single/transactions/mapper/TransactionDBEntityMapper.kt
+++ data/src/main/java/com/jeizard/mbanking/data/room/models/single/transactions/mapper/TransactionDBEntityMapper.kt
@@ -7,11 +7,11 @@ import com.jeizard.mbanking.domain.entities.Transaction
 class TransactionDBEntityMapper : Mapper<TransactionDBEntity, Transaction> {
 
     override fun mapFromDBEntity(d: TransactionDBEntity): Transaction {
-        return Transaction(d.number, d.company, d.date, d.amount, d.status, d.accountNumber)
+        return Transaction(d.number, d.company, d.date, d.amount, d.status)
     }
 
     override fun mapToDBEntity(e: Transaction): TransactionDBEntity {
-        return TransactionDBEntity(e.number, e.company, e.date, e.amount, e.status, e.accountNumber)
+        return TransactionDBEntity(e.number, e.company, e.date, e.amount, e.status)
     }
 
     override fun mapFromDBEntity(dCollection: Collection<TransactionDBEntity>): List<Transaction> {
