1e1e852eb17a3b1743538adf807792f33f1f0a21
Jeizard <japhex@mail.ru>
2024-07-04 22:28:22 +0300

    feat: prepopulate database

--- data/src/main/java/com/jeizard/mbanking/data/room/models/single/accounts/dao/AccountDao.kt
+++ data/src/main/java/com/jeizard/mbanking/data/room/models/single/accounts/dao/AccountDao.kt
@@ -1,11 +1,15 @@
 package com.jeizard.mbanking.data.room.models.single.accounts.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.jeizard.mbanking.data.room.models.single.accounts.entity.AccountDBEntity
 
 @Dao
 interface  AccountDao {
+    @Insert
+    fun insert(accountDBEntity: AccountDBEntity)
+
     @Query("SELECT * FROM accounts")
     fun getAll(): List<AccountDBEntity>
 }
